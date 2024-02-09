e640c887a300e798d589bb5afaaf4a50eb0a2d75
Noumankhalil <nomiikhan103@gmail.com>
2024-02-09 10:08:17 +0500

    cosmatic changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,16 +3,16 @@ import ContactSection from "@/component/contact/ContactSection";
 import Layout from "@/component/layout/Layout";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: 'Cafeu Contact Page',
-    description: 'Developed by Azizur Rahman',
-  }
+  title: " Contact Page",
+  description: "Made by HubSolutions",
+};
 export default function Contact() {
-    return (
-        <div className="wrapper">
-            <Layout>
-                <BreadcrumbSection title="Contact" header="Contact Us"/> 
-                <ContactSection/>
-            </Layout>
-        </div>
-    )
+  return (
+    <div className='wrapper'>
+      <Layout>
+        <BreadcrumbSection title='Contact' header='Contact Us' />
+        <ContactSection />
+      </Layout>
+    </div>
+  );
 }
